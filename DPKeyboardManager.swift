5b2e3a344341712caffa5fd0e2f11e3bdcbdbd85
Danilo <danilo.priore@gmail.com>
2018-02-01 13:22:34 +0100

    fix for tableview autoscroll

--- DPKeyboardManager.swift
+++ DPKeyboardManager.swift
@@ -11,11 +11,15 @@ import Foundation
 
 open class DPKeyboardManager {
     
-    private weak var containerView: UIView?
+    static internal var disableTableViewAutoscoll: Bool = false
     
+    private weak var containerView: UIView?
+    private var keyboardAnimationDuration: Double = 0
+
     @objc open var currentView: UIView?
     @objc open var keyboardRect: CGRect = CGRect.zero
     @objc open var keyboardPadding: CGFloat = 10.0
+
     
     @objc public init() {
         // NOP
@@ -38,6 +42,7 @@ open class DPKeyboardManager {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
     }
     
     @objc open func disableKeyboardEvents() {
@@ -49,20 +54,28 @@ open class DPKeyboardManager {
     
     @objc private func keyboardWillShow(notification:NSNotification) {
         
+        DPKeyboardManager.disableTableViewAutoscoll = true
+        
         let userInfo = notification.userInfo!
         let keyboardFrame:NSValue = userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        let keyboardAnimation: NSNumber = userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber
         self.keyboardRect = keyboardFrame.cgRectValue
+        self.keyboardAnimationDuration = keyboardAnimation.doubleValue
         
         translateTextField()
     }
     
     @objc private func keyboardWillHide(notification:NSNotification) {
-        
-        UIView.animate(withDuration: 0.25) {
-            DispatchQueue.main.async {
+
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: self.keyboardAnimationDuration) {
                 self.containerView?.transform = CGAffineTransform.identity
             }
         }
+    }
+
+    @objc private func keyboardDidHide(notification:NSNotification) {
+        DPKeyboardManager.disableTableViewAutoscoll = false
     }
     
     // MARK: - UITextFiled
@@ -72,26 +85,35 @@ open class DPKeyboardManager {
         translateTextField()
     }
     
-    
-    @objc private func textFieldDidBeginEditingAction(_ textField: UITextField) {
-        self.currentView = textField
-        translateTextField()
-    }
-    
     @objc private func translateTextField() {
         
         if let view = currentView, keyboardRect.height > 0 {
             let rect = view.superview?.convert(view.frame, to: nil)
             if let maxY = rect?.maxY, maxY > keyboardRect.minY {
                 let delta = maxY - keyboardRect.minY + keyboardPadding
-                UIView.animate(withDuration: 0.25) {
-                    DispatchQueue.main.async {
+                DispatchQueue.main.async {
+                    UIView.animate(withDuration: self.keyboardAnimationDuration) {
                         self.containerView?.transform = CGAffineTransform.identity.translatedBy(x: 0, y: -delta)
                     }
                 }
             }
         }
-        
     }
+}
+
+extension UITableView {
+    
+    open override var contentOffset: CGPoint {
+        get {
+            return super.contentOffset
+        }
+        
+        set {
+            if !DPKeyboardManager.disableTableViewAutoscoll {
+                super.contentOffset = newValue
+            }
+        }
+    }
+    
     
 }
